6cd3ab90edfd2ebcd3899a7079c08a6151904b73
Ralph.Vitug <rralph@rrc.ca>
2026-02-25 11:33:58 -0600

    added basic health check

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,23 @@
 import express, { Express } from "express";
+import ticketRoutes from "./api/v1/"
+
 
 // Initialize Express application
 const app: Express = express();
 
 app.use(express.json());
+
+app.use("/api/v1");
+
+// Sample healt check
+app.get("/api/v1/health", (req, res) => {
+    res.json({
+        status: "OK",
+        uptime: process.uptime(),
+        timestamp: new Date().toISOString(),
+        version: "1.0.0",
+    });
+});
 
 // Define a route
 app.get("/", (req, res) => {
