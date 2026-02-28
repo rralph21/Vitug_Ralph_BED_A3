c84e6009e9d1bcbbad15e0851fade70416da032a
Ralph.Vitug <rralph@rrc.ca>
2026-02-28 16:03:07 -0600

    edit: router to use validation and middleware

--- src/api/v1/routes/eventRoutes.ts
+++ src/api/v1/routes/eventRoutes.ts
@@ -1,22 +1,46 @@
 import { Router } from "express";
 import {
-  getEvents,
-  getEvent,
-  getEventPopularity,
-  createEvent,
-  updateEvent,
-  deleteEvent,
+    getEvents,
+    getEvent,
+    getEventPopularity,
+    createEvent,
+    updateEvent,
+    deleteEvent,
 } from "../controllers/eventControllers";
 
+import { validateRequest } from "../middleware/validateRequest";
+import { eventSchemas } from "../validation/eventValidation";
 
 const router = Router();
 
 router.get("/events", getEvents);
-router.get("/events/:id", getEvent);
-router.get("/events/:id/popularity", getEventPopularity);
 
-router.post("/events", createEvent);
-router.put("/events/:id", updateEvent);
+
+router.get(
+    "/events/:id",
+    validateRequest(eventSchemas.getById),
+    getEvent
+);
+
+router.get(
+    "/events/:id/popularity",
+    validateRequest(eventSchemas.getById),
+    getEventPopularity
+);
+
+router.post(
+    "/events",
+    validateRequest(eventSchemas.create),
+    createEvent
+);
+
+
+router.put(
+    "/events/:id",
+    validateRequest(eventSchemas.update),
+    updateEvent
+);
+
 router.delete("/events/:id", deleteEvent);
 
 export default router;
